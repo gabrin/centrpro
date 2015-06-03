19283353ae236357859a63860947022ce62dde51
Rinat <rinat@gabidullin.com>
2015-06-03 19:52:52 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+这是一个Google镜像服务程序, 运行在[Node.js](http://nodejs.org/)环境, 使用[Express](http://expressjs.com/)框架.
+
+## 原理 ##
+使用[Google Custom Search](https://www.google.com/cse)的API.  
+看到[google200](https://github.com/zjuyxy/google200)使用此方法, 借鉴一下.
+
+
+## 使用方法 ##
+1. 首先确保你已经安装了[Node.js](http://nodejs.org/);
+2. 解压后, 执行`npm install`安装依赖库;
+3. `node app.js`开启服务.
+
+## 局限性 ##
+由于API的限制, 搜索结果只可查看最前面的100个.
+[Custom Search Element Control API](https://developers.google.com/custom-search/docs/element)中说到`requests up to 10 results per page, for a maximum of 10 pages or 100 results.`
+# centrpro
---
+++ app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var centrpro = express();
+var favicon = require('serve-favicon');
+var request = require('request');
+var fs = require('fs');
+
+var KEY = 'AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY';
+var CX = '013753918506466391726:hw47szr5hae';
+var REQ_URL = 'https://www.googleapis.com/customsearch/v1element?' +
+                'prettyPrint=false' +
+                '&key=' + KEY +
+                '&cx=' + CX;
+
+centrpro.set('view engine', 'ejs');
+centrpro.set('views', __dirname + '/views');
+
+centrpro.use(favicon(__dirname + '/public/img/favicon.ico'));
+centrpro.use(express.static(__dirname + '/public/'));
+
+centrpro.get('/search', function(req, res, next) {
+  var url;
+  if (req.query.q) {
+    url = REQ_URL;
+
+    for (var i in req.query) {
+      url = url + '&' + i + '=' + req.query[i];
+    }
+
+    request(url, function (err, response, body) {
+      var para;
+
+      if (!err && response.statusCode == 200) {
+        para = JSON.parse(body);
+        if (para.error) {
+          res.render('index');
+        } else {
+          para.q = req.query.q;
+          res.render('search', para);
+        }
+
+      } else {
+        res.render('index');
+      }
+    });
+
+  } else {
+    res.render('index');
+  }
+});
+
+centrpro.get(/^\/(index.html)?/, function(req, res) {
+  res.render('index');
+});
+
+centrpro.listen(process.env.PORT || 5000);
